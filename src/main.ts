162e2d7a711c327745c33effb8e78df96a55e3d0
Jesus Bejar <israelbejarii@gmail.com>
2025-07-05 15:31:49 -0600

    added type, class and constructor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,32 @@
 // define type FIRST, id, make, model, year
-type CarInfo {
-
-}
+type CarInfo = {
+  id: number;
+  make: string;
+  model: string;
+  year: number;
+};
 // define class SECOND
 class Car {
+  // constructors
+  constructor(
+    private id: number,
+    private make: string,
+    private model: string,
+    private year: number
+  ) {
+    (this.id = id),
+      (this.make = make),
+      (this.model = model),
+      (this.year = year);
+  }
 
+  // public displayMake() {
+  //   return `here is the make ${this.make}`;
+  // }
 }
+// const Nissan305Z = new Car(1, "Nissan", "350Z", 2010);
+// console.log(Nissan305Z.displayMake());
+
 // get car info
 function getCarInfo() {}
 
