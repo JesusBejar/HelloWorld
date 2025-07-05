697a3df21d95dceeeb623646ebe725e935b0f61b
Jesus Bejar <israelbejarii@gmail.com>
2025-07-05 15:11:12 -0600

    added file comments

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+// define type FIRST, id, make, model, year
+type CarInfo {
+
+}
+// define class SECOND
+class Car {
+
+}
+// get car info
+function getCarInfo() {}
+
+// display car info
+function displayCarInfo() {}
+
+// init function
+function init() {}
