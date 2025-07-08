01000251c535cc281644ff024faaa250af267fba
Jesus Bejar <israelbejarii@gmail.com>
2025-07-08 16:47:10 -0600

    started strs, functions and got inputs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 // str 1
 // str 2
+let str1 = "";
+let str2 = "";
 
 // main function (str1, str2)
+function longestCommonSubstring(str1, str2) {
 
-// base case
-
-// recursive function (str1, str2, i, j, length)
+  // base case
+  // how many edge cases do I need to add?
+  if (str1.length ===  str2.length) {
+    return 
+  }
+  // recursive function (str1, str2, i, j, length)
+  function findLongestCommonSubstring(str1, str2, i, j, length) {
+  }
+}
 
 // init function??
+function init() {
+  const input1 = document.querySelector("#strOne") as HTMLInputElement;
+  const input2 = document.querySelector("#strTwo") as HTMLInputElement;
+  const btn = document.querySelector("#btn") as HTMLButtonElement;
+
+  btn.addEventListener("click", () => {
+    longestCommonSubstring(str1, str2);
+  });
+}
