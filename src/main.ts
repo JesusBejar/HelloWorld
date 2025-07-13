d6ea587c26d8c64d2df7e9275ce0f6bfd3b4835b
Jesus Bejar <israelbejarii@gmail.com>
2025-07-12 19:12:28 -0600

    got elements

--- src/main.ts
+++ src/main.ts
@@ -1,66 +1,11 @@
-// inputs
-let str1 = "";
-let str2 = "";
+const syntaxBtn = document.querySelector("#btn") as HTMLElement;  
+const runtimeBtn = document.querySelector("#btn") as HTMLElement;  
+const logicBtn = document.querySelector("#btn") as HTMLElement;  
+const typeBtn = document.querySelector("#btn") as HTMLElement;  
+const referenceBtn = document.querySelector("#btn") as HTMLElement;  
 
-// main function (str1, str2)
-function longestCommonSubstring(str1: string, str2: string): string {
-  // max length of common substring (num)
-  let maxLength = 0;
-  // longest common substring (str)
-  let resultSubstring = "";
-  
-  // base case - handle empty or invalid inputs
-  if (!str1 || !str2) {
-    return "please enter two strings"
-  }
-
-  // start w 1st str
-  for (let i = 0; i < str1.length; i++) {
-    // continue w str2
-    for (let j = 0; j < str2.length; j++) {
-            // recursion
-            let currentLength = findLongestCommonSubstring(str1, str2, i, j, 0);
-      
-            // this is the longest match we've found so far
-            if (currentLength > maxLength) {
-              maxLength = currentLength;
-              resultSubstring = str1.substring(i, i + currentLength);
-            }
-    }
-  }
-  
-  return resultSubstring || "No common substring found";
-}
-
-// recursive function
-function findLongestCommonSubstring(str1: string, str2: string, i: number, j: number, currentLength: number): number {
-  // base case 1, reached the end of either str
-  if (i >= str1.length || j >= str2.length) {
-    return currentLength;
-  }
-  
-  // base case 2, characters don't match - stop counting
-  if (str1[i] !== str2[j]) {
-    return currentLength;
-  }
-  
-  // recursive case, characters match! 
-  return findLongestCommonSubstring(str1, str2, i + 1, j + 1, currentLength + 1);
-}
-
-// init function
-function init() {
-  let input1 = document.querySelector("#strOne") as HTMLInputElement;
-  let input2 = document.querySelector("#strTwo") as HTMLInputElement;
-  const btn = document.querySelector("#btn") as HTMLButtonElement;
-  const result = document.querySelector("#result") as HTMLElement;
-
-  btn.addEventListener("click", () => {
-    let str1 = input1.value
-    let str2 = input2.value
-    const answer = longestCommonSubstring(str1, str2);
-    result.textContent = `Longest common substring: "${answer}"`;
-  });
-}
-
-window.onload = init;
+const syntaxError = document.querySelector("#syntax-result") as HTMLElement;
+const runtimeError = document.querySelector("#runtime-result") as HTMLElement;
+const logicError = document.querySelector("#logic-result") as HTMLElement;
+const typeError = document.querySelector("#type-result") as HTMLElement;
+const referenceError = document.querySelector("#reference-result") as HTMLElement;
