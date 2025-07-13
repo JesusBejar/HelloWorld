2187cb437c816fdf5a96c94c21fbf312ea6a0824
Jesus Bejar <israelbejarii@gmail.com>
2025-07-12 19:28:36 -0600

    changed up two error types

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-const syntaxBtn = document.querySelector("#syntax-btn") as HTMLElement;  
-const runtimeBtn = document.querySelector("#runtime-btn") as HTMLElement;  
-const logicBtn = document.querySelector("#logic-btn") as HTMLElement;  
+const syntaxBtn = document.querySelector("#syntax-btn") as HTMLElement;   
+const genericBtn = document.querySelector("#generic-btn") as HTMLElement;  
+const rangeBtn = document.querySelector("#range-btn") as HTMLElement;  
 const typeBtn = document.querySelector("#type-btn") as HTMLElement;  
 const referenceBtn = document.querySelector("#reference-btn") as HTMLElement;  
 
 const syntaxError = document.querySelector("#syntax-result") as HTMLElement;
-const runtimeError = document.querySelector("#runtime-result") as HTMLElement;
-const logicError = document.querySelector("#logic-result") as HTMLElement;
+const genericError = document.querySelector("#generic-result") as HTMLElement;
+const rangeError = document.querySelector("#range-result") as HTMLElement;
 const typeError = document.querySelector("#type-result") as HTMLElement;
 const referenceError = document.querySelector("#reference-result") as HTMLElement;
+
